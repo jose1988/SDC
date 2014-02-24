074ccfbf618884d6457c357c112546948dc68d5d
Niuska Mora <niuskamora@gmail.com>
2014-02-24 17:27:54 -0430

    pdf de correspondencia

---
+++ pages/proof_of_correspondence.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+include("../recursos/funciones.php");
+require_once('../lib/nusoap.php');
+
+/*if(!isset($_SESSION["Usuario"])){	
+	iraURL("index.php");
+}*/
+
+try {
+	$wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
+  	$client = new SOAPClient($wsdl_url);
+  	$client->decode_utf8 = false;
+	
+	$usuario = array('user' => 'niuska.mora');
+	$resultadoConsultarUsuario = $client->consultarUsuarioXUser($usuario);
+	
+	if(!isset($resultadoConsultarUsuario->return)){
+		$usua = 0;
+	}else{
+		$usua = $resultadoConsultarUsuario->return;
+	}
+	
+	$idUsuario = $resultadoConsultarUsuario->return->idusu;
+	
+	try {
+		$wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/Niuska?WSDL';
+  		$client = new SOAPClient($wsdl_url);
+  		$client->decode_utf8 = false;
+	
+		$idUsu = array('idUsuario' => $idUsuario);
+		$resultadoConsultarUltimoPaquete = $client->ultimoPaqueteXOrigen($idUsu);
+		
+		$idSede = array('idSede' => '1');
+		$resultadoConsultarSede = $client->consultarSedeXId($idSede);
+	
+		
+	} catch (Exception $e) {
+		javaalert('Lo sentimos no hay conexión');
+		iraURL('../index.php');	
+	}
+	
+	include("../views/proof_of_correspondence.php");
+	
+} catch (Exception $e) {
+	javaalert('Lo sentimos no hay conexión');
+	iraURL('../index.php');	
+}
+?>
---
+++ views/proof_of_correspondence.php
@@ -0,0 +1,146 @@
+<?php
+require_once("../pdf/dompdf/dompdf_config.inc.php");
+
+$nombre = $resultadoConsultarUltimoPaquete->return->origenpaq->nombreusu;
+$apellido = $resultadoConsultarUltimoPaquete->return->origenpaq->apellidousu;
+
+$nombredes = $resultadoConsultarUltimoPaquete->return->destinopaq->idusubuz->nombreusu;
+$direcciondes = $resultadoConsultarUltimoPaquete->return->destinopaq->idusubuz->direccionusu;
+$telefonodes = $resultadoConsultarUltimoPaquete->return->destinopaq->idusubuz->telefonousu;
+
+$sede = $resultadoConsultarSede->return->nombresed;
+
+# Contenido HTML del documento que queremos generar en PDF.
+$html='
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Comprobante de Correspondencia</title>
+<link rel="stylesheet" href="../recursos/estilosPdf.css" type="text/css" />
+</head>
+<body>
+<br>
+<table align="center" width="500" border="0" >
+  <tr>
+    <td>
+		<img src="../images/header-top-left.png" width="330" height="50">
+   	  	<h2 align="center">Sistema de Correspondencia</h2>
+    	<table width="500" border="0">
+  		<tr>
+    		<td><strong>Sede: </strong>'.$sede.'</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td>&nbsp;</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Realizado por: </strong>'.$nombre.' '.$apellido.'</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td>&nbsp;</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Nombre: </strong>'.$nombredes.'</td>
+    		<td><strong>Dirección: </strong>'.' '.' '.' '.$direcciondes.'</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Teléfono: </strong>'.$telefonodes.'</td>
+    		<td><strong>Lugar de Envio: </strong>'.$sede.'</td>
+  		</tr>
+  		<tr>
+  		  <td colspan="2">&nbsp;</td>
+		</tr>
+		<tr>
+  		  <td colspan="2">&nbsp;</td>
+		</tr>
+  		<tr>
+  		  <td align="center"><strong>________________</strong></td>
+          <td align="center"<strong>________________</strong></td>
+		  </tr>
+  		<tr>
+  		  <td align="center"><strong>Usuario</strong></td>
+          <td align="center"><strong>Recepción</strong></td>
+		</tr>
+	</table>
+	</td>
+  </tr>
+ 
+  <tr>
+  	<td>&nbsp;</td>
+  </tr>
+  <tr>
+  	<td>&nbsp;</td>
+  </tr>
+  <tr>
+  	<td>---------------------------------------------------------------------------------------------------------------------------</td>
+  </tr>
+  <tr>
+  	<td>&nbsp;</td>
+  </tr>
+  <tr>
+  	<td>&nbsp;</td>
+  </tr>
+  
+  <tr>
+    <td>
+		<img src="../images/header-top-left.png" width="330" height="50">
+		<h2 align="center">Sistema de Correspondencia</h2>
+    	<table width="500" border="0">
+  		<tr>
+    		<td><strong>Sede: </strong>'.$sede.'</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td>&nbsp;</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Realizado por: </strong>'.$nombre.' '.$apellido.'</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td>&nbsp;</td>
+    		<td>&nbsp;</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Nombre: </strong>'.$nombredes.'</td>
+    		<td><strong>Dirección: </strong>'.' '.' '.' '.$direcciondes.'</td>
+  		</tr>
+  		<tr>
+    		<td><strong>Teléfono: </strong>'.$telefonodes.'</td>
+    		<td><strong>Lugar de Envio: </strong>'.$sede.'</td>
+  		</tr>
+  		<tr>
+  		  <td colspan="2">&nbsp;</td>
+		</tr>
+		<tr>
+  		  <td colspan="2">&nbsp;</td>
+		</tr>
+  		<tr>
+  		  <td align="center"><strong>________________</strong></td>
+          <td align="center"<strong>________________</strong></td>
+		  </tr>
+  		<tr>
+  		  <td align="center"><strong>Usuario</strong></td>
+          <td align="center"><strong>Recepción</strong></td>
+		</tr>
+	</table>
+    </td>
+  </tr>
+</table> 
+</body>
+</html>
+';
+
+//Obtenemos el código html de la página web que nos interesa
+$dompdf = new DOMPDF();
+//Creamos una instancia a la clase
+$dompdf->load_html($html);
+//Esta línea es para hacer la página del PDF más grande
+$dompdf->set_paper('carta','portrait');
+$dompdf->render();
+$dompdf->stream("Comprobante de Correspondencia.pdf");
+?>
