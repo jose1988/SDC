bf9e8ef6688b2564d107655161dde6a8f1ce6a0c
Niuska Mora <niuskamora@gmail.com>
2014-03-11 16:25:48 -0430

    cambios

--- pages/proof_pouch.php
+++ pages/proof_pouch.php
@@ -3,24 +3,24 @@ session_start();
 include("../recursos/funciones.php");
 require_once('../lib/nusoap.php');
 
-if(!isset($_SESSION["Usuario"])){	
-	iraURL("../index.php");
-}elseif(!usuarioCreado()){
-	iraURL("../pages/create_user.php");
+if (!isset($_SESSION["Usuario"])) {
+    iraURL("../index.php");
+} elseif (!usuarioCreado()) {
+    iraURL("../pages/create_user.php");
 }
 
 $wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
 $client = new SOAPClient($wsdl_url);
 $client->decode_utf8 = false;
-$UsuarioRol= array('idusu' => $_SESSION["Usuario"]->return->idusu,'sede' =>$_SESSION["Sede"]->return->nombresed);
-$SedeRol=$client->consultarSedeRol($UsuarioRol);
+$UsuarioRol = array('idusu' => $_SESSION["Usuario"]->return->idusu, 'sede' => $_SESSION["Sede"]->return->nombresed);
+$SedeRol = $client->consultarSedeRol($UsuarioRol);
 
-if(isset($SedeRol->return)){
-	if($SedeRol->return->idrol->idrol!="4" && $SedeRol->return->idrol->idrol!="5"){
-   		iraURL('../pages/inbox.php');
-   	}
-}else{
-	iraURL('../pages/inbox.php');
+if (isset($SedeRol->return)) {
+    if ($SedeRol->return->idrol->idrol != "4" && $SedeRol->return->idrol->idrol != "5") {
+        iraURL('../pages/inbox.php');
+    }
+} else {
+    iraURL('../pages/inbox.php');
 }
 
 $nomUsuario = $_SESSION["Usuario"]->return->userusu;
@@ -28,44 +28,43 @@ $ideSede = $_SESSION["Sede"]->return->idsed;
 $usuarioBitacora = $_SESSION["Usuario"]->return->idusu;
 
 try {
-	$wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
-  	$client = new SOAPClient($wsdl_url);
-  	$client->decode_utf8 = false;
-	
-	$usuario = array('user' => $nomUsuario);
-	$resultadoConsultarUsuario = $client->consultarUsuarioXUser($usuario);
-	
-	if(!isset($resultadoConsultarUsuario->return)){
-		$usua = 0;
-	}else{
-		$usua = $resultadoConsultarUsuario->return;
-	}
-	
-	$idUsuario = $resultadoConsultarUsuario->return->idusu;
-	
-	try {
-		$wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
-  		$client = new SOAPClient($wsdl_url);
-  		$client->decode_utf8 = false;
-	
-		$idUsu = array('idUsuario' => $idUsuario);
-		$resultadoConsultarUltimaValija = $client->ultimaValijaXUsuario($idUsu);
-		
-		$idSede = array('idSede' => $ideSede);
-		$resultadoConsultarSede = $client->consultarSedeXId($idSede);
-		
-		llenarLog(6, "Comprobante de Valija",$usuarioBitacora,$ideSede);
-		echo"<script language='javascript'>window.location='../pages/create_valise.php';</script>";
-		
-	} catch (Exception $e) {
-		javaalert('Lo sentimos no hay conexion');
-		iraURL('../pages/create_valise.php');
-	}
-	
-	include("../views/proof_pouch.php");
-	
+    $wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
+    $client = new SOAPClient($wsdl_url);
+    $client->decode_utf8 = false;
+
+    $usuario = array('user' => $nomUsuario);
+    $resultadoConsultarUsuario = $client->consultarUsuarioXUser($usuario);
+
+    if (!isset($resultadoConsultarUsuario->return)) {
+        $usua = 0;
+    } else {
+        $usua = $resultadoConsultarUsuario->return;
+    }
+
+    $idUsuario = $resultadoConsultarUsuario->return->idusu;
+
+    try {
+        $wsdl_url = 'http://localhost:15362/SistemaDeCorrespondencia/CorrespondeciaWS?WSDL';
+        $client = new SOAPClient($wsdl_url);
+        $client->decode_utf8 = false;
+
+        $idUsu = array('idUsuario' => $idUsuario);
+        $resultadoConsultarUltimaValija = $client->ultimaValijaXUsuario($idUsu);
+
+        $idSede = array('idSede' => $ideSede);
+        $resultadoConsultarSede = $client->consultarSedeXId($idSede);
+
+        llenarLog(6, "Comprobante de Valija", $usuarioBitacora, $ideSede);
+        echo"<script language='javascript'>window.location='../pages/create_valise.php';</script>";
+        
+    } catch (Exception $e) {
+        javaalert('Lo sentimos no hay conexion');
+        iraURL('../pages/create_valise.php');
+    }
+    include("../views/proof_pouch.php");
+    
 } catch (Exception $e) {
-	javaalert('Lo sentimos no hay conexion');
-	iraURL('../pages/create_valise.php');	
+    javaalert('Lo sentimos no hay conexion');
+    iraURL('../pages/create_valise.php');
 }
 ?>
