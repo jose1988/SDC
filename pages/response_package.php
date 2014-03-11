eb602a10c4f1d10fb2a791215dc2e2b00a47eaca
Mariela Arboleda <luz.arboleda25@gmail.com>
2014-03-11 08:14:52 -0430

    detalle idpaq

--- pages/response_package.php
+++ pages/response_package.php
@@ -72,7 +72,7 @@ $idPadre= array('idpaq' => $_GET['idpaqr']);
 					$paq = array('idpaq' => $idPaquete->return->idpaq);
 					$bandejaorigen=$client->insertarBandejaOrigen($paq);
 					$bandejaDestino=$client->insertarBandejaDestino($paq);
-					$paramPadre = array('idpaq' => $idPaquete->return->idpaq,'status'=>"2");
+					$paramPadre = array('idpaq' => $_GET['idpaqr'],'status'=>"2");
 					$statusPadre=$client->editarEstatusPaquete($paramPadre);
 			if($_FILES['imagen']['name']!=""){
 					$imagenName= $_FILES['imagen']['name'];
