8206874bebdaeb474348032fe17072a4cbcf08c0
Niuska Mora <niuskamora@gmail.com>
2014-03-14 11:34:15 -0430

    cambio de nombre del archivo pdf

--- views/proof_operator_level.php
+++ views/proof_operator_level.php
@@ -244,7 +244,7 @@ if ($contadorPaquetes > 0) {
 //Esta línea es para hacer la página del PDF más grande
     $dompdf->set_paper('carta', 'portrait');
     $dompdf->render();
-    $nom = 'Comprobante Nivel Uno_' . $contadorPaquetes . ' Paquetes.pdf';
+    $nom = 'Comprobante Confirmados_' . $contadorPaquetes . ' Paquetes.pdf';
     $dompdf->stream($nom);
 }//Fin del IF general
 ?>
